7fe0996f2282115533e0e50d05cfd4ed8dc2e66e
20210871 <a.kalais@student.tue.nl>
2026-01-28 18:56:43 +0100

    Add dc2 data and script

---
+++ scripts/dc2_linear_models.R
@@ -0,0 +1,60 @@
+# =====================================================
+# Data Challenges 2 - Linear Models 
+# =====================================================
+library(dplyr)
+library(tidyr)
+library(caret)
+library(stargazer)
+library(Metrics)
+library(olsrr)
+
+# Create directories for saving tables
+dir.create(file.path("tables"), recursive = TRUE)
+
+# -----------------------------
+# Load datasets
+# -----------------------------
+train_dataset <- read.csv("data/dc2_trainDataset.csv")
+test_dataset  <- read.csv("data/dc2_testDataset.csv")
+
+# Print first few rows to check the datasets
+head(train_dataset)
+head(test_dataset)
+
+# -----------------------------
+# Question 1
+# -----------------------------
+
+# -----------------------------
+# Model (a): Only flow
+# -----------------------------
+lm_flow_only <- 
+
+# -------------------------------------------
+# Model (b): All variables except speed_ngh
+# -------------------------------------------
+lm_all_but_speed <- 
+
+# -------------------------------------------
+# Model (c): All variables
+# -------------------------------------------
+lm_all_var <- 
+
+# -------------------------------------------
+# Model (d): Step forward function
+# -------------------------------------------
+step_forward_model <- 
+
+# -------------------------------------------
+# Summary Table
+# -------------------------------------------
+
+
+# -----------------------------
+# Question 2
+# -----------------------------
+
+
+# -----------------------------
+# Question 3
+# -----------------------------
