b8a8d8c424c715ac29ad4cefd6ca0f41d5ca05f5
stieterd <peviho@outlook.com>
2026-02-11 20:41:11 +0100

    tasks renamed

--- main.R
+++ main.R
@@ -148,20 +148,15 @@ metrics_table <- make_metrics_table(
 
 
 # -----------------------------
-# Question 3 - Task 11
+# Question 3 - Task 12
 # -----------------------------
 
 # Get the counts of congestion in the dataset (number of 0 and 1 entries)
 counts <- table(train_dataset$congestion)
 print(counts)
 
-# Calculate the imbalance ratio (majority class / minority class)
-imbalance_ratio <- max(counts) / min(counts)
-print(imbalance_ratio)
-
-
 # -----------------------------
-# Question 4 - Task 14
+# Question 4 - Task 15
 # -----------------------------
 
 # Add predictions of each model to the test dataset as a new column
